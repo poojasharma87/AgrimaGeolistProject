39de690b84502963f0fb976e05c733cee3a5f5a5
poojasharma87 <69187568+poojasharma87@users.noreply.github.com>
2021-02-11 13:43:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AgrimaGeolistProject
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ rubber.js
@@ -0,0 +1,19 @@
+class Rubber {
+    constructor(x, y, radius) {
+      var options = {
+          'restitution':0.3,
+          'friction':5,
+          'density':1.0
+      }
+      this.radius= radius;
+     this.body = Matter.Bodies.circle(x, y, radius);
+     World.add(world,this.body);
+      
+     
+    }
+    display(){
+     fill("blue");
+      ellipse(this.body.position.x, this.body.position.y, this.radius);
+    
+    }
+  };
